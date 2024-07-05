cb0402e0e124c3dd5a2dc5713a2b0a5af18fa0a5
yanchevskaya <aliceyan@yandex.ru>
2024-07-05 20:13:29 +0300

    Refactor factorial method and factorialTest

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,11 +1,34 @@
-import lesson_10.Factorial;
 import org.testng.annotations.Test;
+
+import java.math.BigInteger;
+
+import static lesson_10.Factorial.factorial;
 import static org.testng.Assert.assertEquals;
 
 public class FactorialTest {
 
+    @Test
+    void testFactorialOne () {
+        assertEquals(factorial(1), BigInteger.valueOf(1));
+    }
+
+    @Test
+    void testFactorialNull (){
+        assertEquals(factorial(0), BigInteger.valueOf(1));
+    }
+
+    @Test
+    void testFactorialTen () {
+        assertEquals(factorial(10), BigInteger.valueOf(3628800));
+    }
+
+    @Test
+    void testFactorialTwenty () {
+        assertEquals(factorial(20), new BigInteger("2432902008176640000"));
+    }
+
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testFactorialNegative() {
-        Factorial.factorial(-1);
+    void testFactorialNegative() {
+        factorial(-1);
     }
 }
